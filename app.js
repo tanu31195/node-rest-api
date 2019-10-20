567ce6646208d18178f23dddbf170ed6484e2e54
Tanushka PC <tanu31195@users.noreply.github.com>
2019-10-20 16:14:00 +0530

    Added copyright profile

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/*
+ * Created by Tanushka Bandara (https://tanu31195.github.io)
+ * Last Modified on 10/20/19, 4:13 PM
+ * Copyright (c) 2019. All rights reserved.
+ */
+
 const express = require('express');
 
 const app = express();
@@ -5,6 +11,9 @@ const app = express();
 //Route
 app.get('/', (req, res) => {
     res.send('<H1>Hello World</H1>');
+});
+app.get('/posts', (req, res) => {
+    res.send('<H1>Posts</H1>');
 });
 
 //Start listening to server
